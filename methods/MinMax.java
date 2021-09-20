5cd212d51750d8de6ad24c08e30a879f6e499264
Prashant <54601213+prashantkr006@users.noreply.github.com>
2021-09-20 11:41:52 +0530

    Create MinMax.java

---
+++ methods/MinMax.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class MinMax {
+
+    static  int minNum(int a, int b, int c){
+        int min = Math.min(Math.min(a, b), c);
+        return  min;
+    }
+
+    static int maxNum(int a, int b, int c){
+        int max = Math.max(Math.min(a, b), c);
+        return max;
+    }
+    public static void main(String[] args) {
+        int a = 10, b = 3, c = 19;
+
+        System.out.println("minimum is "+minNum(a, b, c));
+        System.out.println("maximum is "+maxNum(a ,b, c));
+    }
+}
