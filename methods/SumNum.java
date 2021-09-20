44542fd8944f84d33d320672a0acc12053f918ab
Prashant <54601213+prashantkr006@users.noreply.github.com>
2021-09-20 12:12:56 +0530

    Add files via upload

---
+++ methods/SumNum.java
@@ -0,0 +1,17 @@
+package methods;
+import java.util.*;
+
+
+public class SumNum {
+
+    static int sum(int num1, int num2){
+        int sum = num1 + num2;
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println(sum(num1, num2));
+    }
+}
