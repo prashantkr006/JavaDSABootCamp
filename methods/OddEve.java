ae6d6468e4d6fed49d58d5299be22936187e97cc
Prashant <54601213+prashantkr006@users.noreply.github.com>
2021-09-20 11:48:23 +0530

    Add files via upload

---
+++ methods/OddEve.java
@@ -0,0 +1,13 @@
+public class OddEve {
+
+    static  void isOddEve(int num){
+        if(num % 2 == 0){
+            System.out.println("even");
+        }
+        else
+            System.out.println("Odd");
+    }
+    public static void main(String[] args) {
+        isOddEve(9);
+    }
+}
