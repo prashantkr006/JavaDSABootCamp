a297734cd3d5db2bee65bf5be3fd9f54c22bad0a
Prashant <54601213+prashantkr006@users.noreply.github.com>
2021-09-20 12:03:18 +0530

    Add files via upload

---
+++ methods/CanVote.java
@@ -0,0 +1,13 @@
+public class CanVote {
+
+    static void isVoter(int age){
+        if(age >= 18)
+            System.out.println("Eligible for vote");
+        else
+            System.out.println("Not eligble");
+    }
+    public static void main(String[] args) {
+        isVoter(20);
+
+    }
+}
