19449a27430c080961b30b8e8c0e16d7f44342ca
Prashant <54601213+prashantkr006@users.noreply.github.com>
2021-09-20 12:28:58 +0530

    Add files via upload

---
+++ methods/Circle.java
@@ -0,0 +1,21 @@
+package methods;
+
+import java.util.Scanner;
+
+public class Circle {
+    static double circmference(float radius){
+        double circumference = 2 * Math.PI * radius;
+        return circumference;
+    }
+
+    static double area(float radius){
+        double area = Math.PI * radius * radius;
+        return area;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float radius = sc.nextFloat();
+
+        System.out.println("Area of circle of radius "+ radius + " is " + String.format("%.2f", area(radius)) + "\n and circumference is " + String.format("%.2f", circmference(radius)));
+    }
+}
