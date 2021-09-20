fd920c188924e980468ff864d7daccfcba152ab5
Prashant <54601213+prashantkr006@users.noreply.github.com>
2021-09-20 11:00:33 +0530

    Add files via upload

---
+++ Insertion.java
@@ -0,0 +1,26 @@
+//Program to insert an element at given index of an array
+package Arrays;
+
+import java.util.*;
+
+public class Insertion {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] ar = {5,9,12,3,4};
+		
+		for(int x: ar) {
+			System.out.print(x+" "); //Array Before Insertion
+		}
+		
+		//Inserting element 
+		int element, index;
+		System.out.println("enter the element");
+		element = sc.nextInt();
+		System.out.println("enter the Indecx value");
+		index = sc.nextInt();
+		
+
+	}
+
+}
