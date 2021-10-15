fc9a53ad9f524ec2e5c0f84149c162c6801a4060
Prashant <54601213+prashantkr006@users.noreply.github.com>
2021-10-15 10:20:16 +0530

    Create floor.java

---
+++ Searching/Problems/floor.java
@@ -0,0 +1,25 @@
+//program to find greatest number smaller than target in an array
+public class Main
+{
+	static int ceiling(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end){
+            int mid = (start  + end) / 2;
+            if(arr[mid] == target)
+                return mid;
+                
+            else if(arr[mid] < target)
+                start = mid + 1;
+            else 
+                end = mid - 1;
+        }
+        return end;
+    }
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 9, 14, 16, 18};
+        int target = 15;
+        System.out.println(ceiling(arr, target));
+    }
+}
