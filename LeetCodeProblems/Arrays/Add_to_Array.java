246a5c002c8f1b041d3e5fa3b2e5512c3651b71d
prashantkr006 <prashantkr006@gmail.com>
2022-02-03 22:51:39 +0530

    added some more leetCode array easy problems

--- LeetCodeProblems/Arrays/Add_to_Array.java
+++ LeetCodeProblems/Arrays/Add_to_Array.java
@@ -1,21 +1,26 @@
 package EasyProblems;
-
 import java.util.*;
 
 public class Add_to_Array {
     public static void main(String[] args) {
         int[] num = {2,1,5};
         int k = 806;
-        System.out.println(addToArrayForm(num,k));
+        System.out.println(addToArrayForm(num, k));
+
     }
     static public List<Integer> addToArrayForm(int[] num, int k) {
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        for(int i = num.length-1; i >= 0; i--){
-            list.add(0, (num[i]+k) % 10);
-            k = (num[i]+k) / 10;
+        int i = num.length - 1;
+        List<Integer> list = new ArrayList<>();
+        while(i >= 0 || k > 0) {
+            if(i >= 0) {
+                list.add(0, (num[i] + k) % 10);
+                k  = (num[i] + k) / 10;
+            } else if(k > 0) {
+                list.add(0, k % 10);
+                k /= 10;
+            }
+            i--;
         }
-        if(k != 0)
-            list.add(0,k);
         return list;
     }
 }
