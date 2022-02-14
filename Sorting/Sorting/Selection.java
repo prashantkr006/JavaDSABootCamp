bd790539790b69700a7527724a3934793181d15b
prashantkr006 <prashantkr006@gmail.com>
2022-02-14 10:15:39 +0530

    added selection sort files

---
+++ Sorting/Sorting/Selection.java
@@ -0,0 +1,48 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class Selection {
+    public static void main(String[] args) {
+        int[] arr = {7, 2, 9, 6, 1, 5, 4};
+        sortAscending(arr);
+        System.out.println(Arrays.toString(arr));
+        sortDescending(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void sortAscending(int[] arr){
+        // sort in ascending order
+        for(int i = 0; i < arr.length-1; i++) { //0 to length-2
+            int min = arr[i]; // assuming arr[i] is minimum for now
+            int minIndex = i; // saving its Index
+            for(int j = i+1; j < arr.length; j++) { // i+1 to length - 1
+                if(arr[j] < min) {
+                    min = arr[j]; // update it with minimum value
+                    minIndex = j; // update the index of minimum
+                }
+            }
+            swap(arr, i, minIndex); //swap minimum value to the ith value
+        }
+    }
+
+    static void sortDescending(int[] arr) {
+        // sorting in descenginf order
+        for(int i = 0; i < arr.length-1; i++) { // 0 to length - 2
+            int max = arr[i]; // assuming arr[i] is the maximum
+            int maxIndex = i; // save index of max
+            for(int j = i+1; j < arr.length; j++) {
+                if(arr[j] > max){
+                    max = arr[j];
+                    maxIndex = j;
+                }
+            }
+            swap(arr, i, maxIndex);
+        }
+    }
+
+    private static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}
