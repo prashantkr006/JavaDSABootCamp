df30d46fd9bf2474b0c86a57488374caa37ea503
prashantkr006 <prashantkr006@gmail.com>
2022-03-05 10:29:05 +0530

    aded recursion easy code

--- Recursion/Com/Easy/Main.java
+++ Recursion/Com/Easy/Main.java
@@ -1,5 +1,14 @@
 package Recursion.Com.Easy;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(sum(10));
+    }
+    static int sum(int n) {
+        if (n == 0)
+            return n;
+        
+        return n + sum(n - 1);
+    }
     
 }
